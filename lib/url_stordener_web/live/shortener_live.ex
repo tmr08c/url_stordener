e5afd16c421ab4bd648625c9b28bd18846c60d59
Troy Rosenberg <tmr08c@gmail.com>
2023-02-24 10:59:15 -0500

    Update ShortenerLive to validate the given URL
    Add a basic initial implementation validating the given URL to shorten
    is shaped like a URL.
    
    We would want to consider extending and hardening this approach. See the
    following for some ideas:
    
    - https://elixirforum.com/t/please-how-do-you-validate-url-inputs-in-your-phoenix-projects-forms/28268/18
    - https://stackoverflow.com/questions/39041335/how-to-validate-url-in-elixir

--- lib/url_stordener_web/live/shortener_live.ex
+++ lib/url_stordener_web/live/shortener_live.ex
@@ -27,12 +27,27 @@ defmodule UrlStordenerWeb.ShortenerLive do
     """
   end
 
-  def url_mapper_changeset(params \\ %{}) do
+  defp url_mapper_changeset(params) do
     types = %{destination_url: :string}
 
     {%{}, types}
     |> Ecto.Changeset.cast(params, Map.keys(types))
     |> Ecto.Changeset.validate_required(Map.keys(types))
+    |> validate_url()
     |> Map.put(:action, :validate)
+  end
+
+  # TODO It's probably worth having a few more tests for this In practice, I
+  # would probably want to consider exploring an existing solution to harden my
+  # approach. Consider reviewing:
+  # https://elixirforum.com/t/please-how-do-you-validate-url-inputs-in-your-phoenix-projects-forms/28268/18
+  defp validate_url(changeset) do
+    with {:ok, url} <- Ecto.Changeset.fetch_change(changeset, :destination_url),
+         uri <- URI.parse(url),
+         true <- is_nil(uri.scheme) or is_nil(uri.host) do
+      Ecto.Changeset.add_error(changeset, :destination_url, "is not a valid URL")
+    else
+      _ -> changeset
+    end
   end
 end
--- test/url_stordener_web/live/shortener_live_test.exs
+++ test/url_stordener_web/live/shortener_live_test.exs
@@ -8,5 +8,21 @@ defmodule UrlStordenerWeb.ShortenerLiveTest do
 
     assert view |> element("form") |> render_submit(%{url_mapper: %{destination_url: ""}}) =~
              "can&#39;t be blank"
+
+    assert view
+           |> element("form")
+           |> render_submit(%{url_mapper: %{destination_url: "not a URL"}}) =~
+             "is not a valid URL"
+
+    refute view
+           |> element("form")
+           |> render_submit(%{
+             url_mapper: %{
+               destination_url:
+                 "https://www.google.com/search?q=url+shortener&oq=google+u&aqs=chrome.0.69
+i59j69i60l3j0j69i57.1069j0j7&sourceid=chrome&ie=UTF-8"
+             }
+           }) =~
+             "is not a valid URL"
   end
 end
