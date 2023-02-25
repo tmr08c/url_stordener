21700286413675be66e2b74720d40b0ba7219a64
Troy Rosenberg <tmr08c@gmail.com>
2023-02-25 10:21:18 -0500

    Remove generated PageController
    We are not using the generated PageController, instead opting for our
    own LiveView.

--- lib/url_stordener_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule UrlStordenerWeb.PageController do
-  use UrlStordenerWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
--- lib/url_stordener_web/controllers/page_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule UrlStordenerWeb.PageHTML do
-  use UrlStordenerWeb, :html
-
-  embed_templates "page_html/*"
-end
