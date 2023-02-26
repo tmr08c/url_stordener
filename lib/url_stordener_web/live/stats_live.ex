424566a83cd27e7cbe018eb7c739aa5d49b147c1
Troy Rosenberg <tmr08c@gmail.com>
2023-02-26 11:05:10 -0500

    Create stats page
    Create a new LiveView to handle "stats." For now, this is simply a table
    listing mappings.

---
+++ lib/url_stordener_web/live/stats_live.ex
@@ -0,0 +1,27 @@
+defmodule UrlStordenerWeb.StatsLive do
+  use UrlStordenerWeb, :live_view
+
+  alias UrlStordener.Stats
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, mappings: Stats.list_mappings())}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <%= if @mappings == [] do %>
+      No shortened URLs have been created yet.
+      <.link navigate={~p"/"} class="font-semibold leading-6 text-zinc-900 hover:text-zinc-700">
+        Create one!
+      </.link>
+    <% else %>
+      <.table id="mappings" rows={@mappings}>
+        <:col :let={mapping} label="Slug">
+          <%= UrlStordenerWeb.Endpoint.url() <> "/" <> mapping.slug %>
+        </:col>
+        <:col :let={mapping} label="Destination"><%= mapping.destination_url %></:col>
+      </.table>
+    <% end %>
+    """
+  end
+end
---
+++ test/url_stordener_web/live/stats_live_test.exs
@@ -0,0 +1,19 @@
+defmodule UrlStordenerWeb.StatsLiveTest do
+  use UrlStordenerWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+
+  test "a useful message appears when there are no URL Mappings", %{conn: conn} do
+    {:ok, _, html} = live(conn, ~p"/stats")
+
+    assert html =~ "No mappings"
+  end
+
+  test "a table of mappings are displayed showing shortened and destination URL", %{conn: conn} do
+    mapping = insert!(:url_mapping)
+    {:ok, _, html} = live(conn, ~p"/stats")
+
+    assert html =~ mapping.destination_url
+    assert html =~ UrlStordenerWeb.Endpoint.url() <> "/" <> mapping.slug
+  end
+end
